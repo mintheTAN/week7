b48f2b13828ae6c0bcccdcc75c6976e5536a6f6e
Minthe Tan <mtan8951@uni.sydney.edu.au>
2023-09-23 23:16:54 +1000

    i updated

--- IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
+++ IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
@@ -1,80 +1,25 @@
-//We need a variable to hold our image
-let img;
+//This is global variable to hold our image
+let logoImage;
+//two variables to hold the width and height of the image
+let logoWidth, logoHeight;
 
-//We will divide the image into segments
-let numSegments = 50;
-
-//We will store the segments in an array
-let segments = [];
-
-//lets add a variable to switch between drawing the image and the segments
-let drawSegments = true;
-
-//lets load the image from disk
+//We will use the preload function, this runs before anything else and it blocks activity until all assets are done loading
 function preload() {
-  img = loadImage('/assets/Mona_Lisa_by_Leonardo_da_Vinci_500_x_700.jpg');
+  //We need to load the image from disk 
+  logoImage = loadImage("assets/picture1.png"); 
 }
 
 function setup() {
-  //We will make the canvas the same size as the image using its properties
-  createCanvas(img.width, img.height);
-  //We can use the width and height of the image to calculate the size of each segment
-  let segmentWidth = img.width / numSegments;
-  let segmentHeight = img.height / numSegments;
-  /*
-  Divide the original image into segments, we are going to use nested loops
-  */
-
-  for (let segYPos=0; segYPos<img.height; segYPos+=segmentHeight) {
-    //this is looping over the height
-    for (let segXPos=0; segXPos<img.width; segXPos+=segmentWidth) {
-      //We will use the x and y position to get the colour of the pixel from the image
-      //lets take it from the centre of the segment
-      let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
-       let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
-       segments.push(segment);
-    }
-  }
+  createCanvas(800, 600);
+  // Set the image dimensions
+  logoWidth = 100;
+  logoHeight = 60;
 }
 
 function draw() {
-  background(0);
-  if (drawSegments) {
-    //lets draw the segments to the canvas
-    for (const segment of segments) {
-      segment.draw();
-    }
-  } else {
-    //lets draw the image to the canvas
-    image(img, 0, 0);
-  }
-}
-function keyPressed() {
-  if (key == " ") {
-    //this is a neat trick to invert a boolean variable,
-    //it will always make it the opposite of what it was
-    drawSegments = !drawSegments;
-  }
-}
+  //set the background to blue
+  background(0, 0, 255);
+  //draw the image in the center of the canvas, we will offset the image by half of its width and height
+  image(logoImage, width / 2 - (logoWidth/2), height / 2 - (logoHeight/2), logoWidth, logoHeight);
 
-
-//Here is our class for the image segments, we start with the class keyword
-class ImageSegment {
-
-  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
-    //these parameters are used to set the internal properties of an instance of the segment
-    //These parameters are named as imageSource as they are derived from the image we are using
-    this.srcImgSegXPos = srcImgSegXPosInPrm;
-    this.srcImgSegYPos = srcImgSegYPosInPrm;
-    this.srcImgSegWidth = srcImgSegWidthInPrm;
-    this.srcImgSegHeight = srcImgSegHeightInPrm;
-    this.srcImgSegColour = srcImgSegColourInPrm;
-  }
-
-  draw() {
-    //lets draw the segment to the canvas, for now we will draw it as an empty rectangle so we can see it
-    stroke(0);
-    fill(this.srcImgSegColour);
-    rect(this.srcImgSegXPos, this.srcImgSegYPos, this.srcImgSegWidth, this.srcImgSegHeight);
-  }
 }
