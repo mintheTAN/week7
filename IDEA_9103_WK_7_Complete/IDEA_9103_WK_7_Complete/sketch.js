1c0631f727c0ee1d2f8aa57c07a08570b178d30b
Minthe Tan <mtan8951@uni.sydney.edu.au>
2023-09-23 23:49:19 +1000

    up

--- IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
+++ IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
@@ -1,25 +1,21 @@
-//This is global variable to hold our image
-let logoImage;
-//two variables to hold the width and height of the image
-let logoWidth, logoHeight;
+let img;
+let imgMask;
 
-//We will use the preload function, this runs before anything else and it blocks activity until all assets are done loading
 function preload() {
-  //We need to load the image from disk 
-  logoImage = loadImage("assets/picture1.png"); 
+  // 加载图像及图像遮罩
+  img = loadImage('assets/moonwalk.jpg');
+  imgMask = loadImage('assets/mask.png');
 }
 
 function setup() {
-  createCanvas(800, 600);
-  // Set the image dimensions
-  logoWidth = 100;
-  logoHeight = 60;
+  createCanvas(720, 400);
+  // mask() 函数将图像遮罩覆盖在图像上
+  img.mask(imgMask);
+  imageMode(CENTER);
 }
 
 function draw() {
-  //set the background to blue
-  background(0, 0, 255);
-  //draw the image in the center of the canvas, we will offset the image by half of its width and height
-  image(logoImage, width / 2 - (logoWidth/2), height / 2 - (logoHeight/2), logoWidth, logoHeight);
-
+  background(0, 102, 153);
+  image(img, width / 2, height / 2);
+  image(img, mouseX, mouseY);
 }
