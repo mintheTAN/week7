745e6d16f901e53273298bfb2bce21867331f98c
Minthe Tan <mtan8951@uni.sydney.edu.au>
2023-09-24 00:10:50 +1000

    change

--- IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
+++ IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
@@ -4,7 +4,7 @@ let easing = 0.05;
 
 function setup() {
   createCanvas(800, 800);
-  img = loadImage('assets/Edvard_Munch_The_Scream.jpeg'); // 加载图像
+  img = loadImage('assets/Edvard_Munch_The_Scream.jpeg'); 
 }
 
 function draw() {
@@ -13,4 +13,51 @@ function draw() {
   offset += dx * easing;
   tint(255, 127); 
   image(img, offset, 0);
+}
+
+function calculateImageDrawProps() {
+
+  //if the image is wider than the canvas
+  if (imgDrwPrps.aspect > canvasAspectRatio) {
+    //then we will draw the image to the width of the canvas
+    imgDrwPrps.width = width;
+    //and calculate the height based on the aspect ratio
+    imgDrwPrps.height = width / imgDrwPrps.aspect;
+    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
+    imgDrwPrps.xOffset = 0;
+  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
+    //otherwise we will draw the image to the height of the canvas
+    imgDrwPrps.height = height;
+    //and calculate the width based on the aspect ratio
+    imgDrwPrps.width = height * imgDrwPrps.aspect;
+    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2;
+    imgDrwPrps.yOffset = 0;
+  }
+  else if (imgDrwPrps.aspect == canvasAspectRatio) {
+    //if the aspect ratios are the same then we can draw the image to the canvas size
+    imgDrwPrps.width = width;
+    imgDrwPrps.height = height;
+    imgDrwPrps.xOffset = 0;
+    imgDrwPrps.yOffset = 0;
+  }
+}
+//Here is our class for the image segments, we start with the class keyword
+class ImageSegment {
+
+  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
+    //these parameters are used to set the internal properties of an instance of the segment
+    //These parameters are named as imageSource as they are derived from the image we are using
+    this.srcImgSegXPos = srcImgSegXPosInPrm;
+    this.srcImgSegYPos = srcImgSegYPosInPrm;
+    this.srcImgSegWidth = srcImgSegWidthInPrm;
+    this.srcImgSegHeight = srcImgSegHeightInPrm;
+    this.srcImgSegColour = srcImgSegColourInPrm;
+  }
+
+  draw() {
+    //let's draw the segment to the canvas, for now we will draw it as an empty rectangle so we can see it
+    stroke(0);
+    fill(this.srcImgSegColour);
+    rect(this.srcImgSegXPos, this.srcImgSegYPos, this.srcImgSegWidth, this.srcImgSegHeight);
+  }
 }
